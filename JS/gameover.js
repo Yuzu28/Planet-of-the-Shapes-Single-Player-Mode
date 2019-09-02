21d0c00e83ab75c5820c24018cc6e6bf3a9a4d92
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2019-09-02 00:20:44 -0400

    single mode

---
+++ JS/gameover.js
@@ -0,0 +1,25 @@
+//show the user score on the screen once they use up all their three lives
+
+function overPopUp() {
+  $("#score").html(score);
+  // if(playerScore<score){
+  //   fetch('/sethighscore',{
+  //     method: 'POST',
+  //     headers: new Headers({
+  //       'Content-Type': 'application/json',
+  //     }),
+  //     body:JSON.stringify({'displayName':playerName, 'highscore':score})
+  //   })
+  // }
+  $("#game-over").show();
+  lvlMusic[level].stop();
+}
+
+function gameOver() {
+  ship.dead = true;
+  text = "Mission Failed ";
+  myAudio.pause();
+  myy.play();
+  overPopUp();
+  hakeem.stop()
+}
